babc60c66dc99edadd25d4345e7e38b712b08dad
cbspace <24204459+cbspace@users.noreply.github.com>
2022-10-12 06:01:52 +1000

    Read file

--- rust8/src/main.rs
+++ rust8/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::Read;
+use std::io;
 
 fn main() {
     let mut pc: u16 = 0;
@@ -9,9 +9,17 @@ fn main() {
     let mut memory: Vec<u8> = vec![0; 0x10000];
     let mut stack: Vec<u16> = vec![];
 
-    let rom_code = load_file("../../assembler/roms/test.c8");
+    let rom_code = load_file("../roms/test.c8");
+    match rom_code {
+        Ok(_) => println!("Ok"),
+        Err(_) => println!("Err")
+    };
 }
 
-fn load_file(file_path: &str) -> Vec<u8> {
-
+fn load_file(file_path: &str) -> Result<Vec<u8>, io::Error> {
+    let file_read_result = std::fs::read(file_path);
+    match file_read_result {
+        Ok(bytes) => Ok(bytes),
+        Err(error) => Err(error),
+    }
 }
