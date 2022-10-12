75ff657100d6726349a48a57a3413917266b0872
cbspace <24204459+cbspace@users.noreply.github.com>
2022-10-13 06:42:54 +1000

    Clean up code

--- rust8/src/main.rs
+++ rust8/src/main.rs
@@ -2,30 +2,62 @@ use std::fs::File;
 use std::io;
 
 fn main() {
-    let mut pc: u16 = 0;
+    let mut pc: u16 = 0x200;
     let mut vreg: Vec<u8> = vec![0; 16];
     let mut ireg: u16 = 0;
 
     let mut memory: Vec<u8> = vec![0; 0x10000];
     let mut stack: Vec<u16> = vec![];
 
-    let rom_code = load_file("../roms/test.c8");
-    match rom_code {
+    load_font_set(&mut memory);
+
+    //let rom_contents = load_file("../roms/test.c8", &mut memory);
+    match load_file("../roms/test.c8", &mut memory) {
+        Ok(()) => {},
+        Err(error) => println!("Error: {}", error)
+    };
+}
+
+fn load_file(file_path: &str, memory: &mut Vec<u8>) -> Result<(), io::Error> {
+    let file_read_result = std::fs::read(file_path);
+    match file_read_result {
         Ok(bytes) => {
             let mut address = 0x200;
             for byte in bytes {
                 memory[address] = byte;
                 address += 1;
             }
+            Ok(())
         },
-        Err(error) => println!("Error: {}", error)
-    };
+        Err(error) => Err(error)
+    }
 }
 
-fn load_file(file_path: &str) -> Result<Vec<u8>, io::Error> {
-    let file_read_result = std::fs::read(file_path);
-    match file_read_result {
-        Ok(bytes) => Ok(bytes),
-        Err(error) => Err(error),
+fn load_font_set(memory: &mut Vec<u8>) {
+    let chip8_fontset: Vec<u8> = vec![
+        0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+        0x20, 0x60, 0x20, 0x20, 0x70, // 1
+        0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+        0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+        0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+        0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+        0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+        0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+        0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+        0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+        0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+        0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+        0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+        0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+        0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+        0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+    ];
+
+    for (index, byte) in chip8_fontset.iter().enumerate() {
+        memory[0x50 + index] = *byte;
     }
+}
+
+fn cpu_cycle() {
+
 }
