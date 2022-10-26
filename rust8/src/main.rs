6adf4773764b65ee5451227867e2621a7c1bea94
cbspace <24204459+cbspace@users.noreply.github.com>
2022-10-26 21:01:49 +1000

    Fixed formatting

--- rust8/src/main.rs
+++ rust8/src/main.rs
@@ -173,9 +173,9 @@ fn cpu_cycle(pc: &mut u16, vreg: &mut Vec<u8>,ireg: &mut u16, memory: &mut Vec<u
     match n1 {
         0x0000 => { 
             match ins & 0x0fff {
-                0x000 => *pc += 2,         // 0NNN Call RCA 1802 program at address NNN
-                0x0E0 => *pc += 2,         // 00E0 Clear the screen
-                0x0EE => {                 // 00EE Return from subroutine
+                0x000 => *pc += 2,    // 0NNN Call RCA 1802 program at address NNN
+                0x0E0 => *pc += 2,    // 00E0 Clear the screen
+                0x0EE => {            // 00EE Return from subroutine
                     let stack_value = stack.pop();
                     match stack_value {
                         Some(v) => *pc = v + 2,
@@ -189,136 +189,146 @@ fn cpu_cycle(pc: &mut u16, vreg: &mut Vec<u8>,ireg: &mut u16, memory: &mut Vec<u
                 }
             }
         },
-        0x1000 => *pc = ins & 0x0fff,        // 1NNN - Jump to NNN
-        0x2000 => { stack.push(*pc);         // 2NNN Call subroutine at address NNN
-                    *pc = ins & 0x0fff;
-                  },        
-        0x3000 => { *pc += if vreg[n2] == (ins & 0xff) as u8 
-                    { 4 } else { 2 };        // 3XNN Skip next instruction if VX = NN   
-                  },                         
-        0x4000 => { *pc += if vreg[n2] != (ins & 0xff) as u8 
-                    { 4 } else { 2 };        // 4XNN Skip next instruction if VX != NN   
-                  },
-        0x5000 => { *pc += if vreg[n2] == vreg[n3]
-                    { 4 } else { 2 };        // 5XY0 Skip next instruction if VX = VY                    
-                  },
-        0x6000 => {                          // 6XNN - Set VX to NN
-                    vreg[n2] = (ins & 0x00ff) as u8; 
-                    *pc += 2; 
-                  },
-        0x7000 => {                         // 7XNN Add NN to VX (Carry flag not changed)  
-                    vreg[n2] = vreg[n2] + (ins & 0xff) as u8;
-                    *pc += 2; 
-                  },
-        0x8000 => { match n4 {
-                        0x0 => {            // 8XY0 Sets the value of VX to VY      
-                            vreg[n2] = vreg[n3];
-                            *pc += 2;
-                        },
-                        0x1 => {            // 8XY1 Sets VX to VX or VY (Bitwise OR operation) 
-                            vreg[n2] |= vreg[n3];
-                            *pc += 2;
-                        },
-                        0x2 => {            // 8XY2 Sets VX to VX & VY (Bitwise AND operation) 
-                            vreg[n2] &= vreg[n3];
-                            *pc += 2;
-                        },
-                        0x3 => {            // 8XY3 Sets VX to VX XOR VY (Bitwise OXR operation) 
-                            vreg[n2] ^= vreg[n3];
-                            *pc += 2;
-                        },
-                        0x4 => {            // 8XY4 Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't. 
-                            let sum: u16 = vreg[n2] as u16 + vreg[n3] as u16;
-                            vreg[0xf] = if sum > 255 { 1 } else { 0 };
-                            vreg[n2] = sum as u8;
-                            *pc += 2;
-                        },
-                        0x5 => {            // 8XY5 VX = VX - VY. VF is set to 0 when there's a borrow, and 1 when there isn't.
-                            let difference: i16 = vreg[n2] as i16 - vreg[n3] as i16;
-                            if difference >= 0 {
-                                vreg[n2] = difference as u8;
-                                vreg[0xf] = 1;
-                            } else {
-                                vreg[n2] = (difference + 256) as u8;
-                                vreg[0xf] = 0;
-                            }
-                            *pc += 2;
-                        },
-                        0x6 => {            // 8XY6 Stores the least significant bit of VX in VF and then shifts VX to the right by 1 
-                            vreg[0xf] = vreg[n2] & 0x01;
-                            vreg[n2] >>= 1; 
-                            *pc += 2;
-                        },
-                        0x7 => {            // 8XY7 VX = VY - VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
-                            let difference: i16 = vreg[n3] as i16 - vreg[n2] as i16;
-                            if difference >= 0 {
-                                vreg[n2] = difference as u8;
-                                vreg[0xf] = 1;
-                            } else {
-                                vreg[n2] = (difference + 256) as u8;
-                                vreg[0xf] = 0;
-                            }
-                            *pc += 2;
-                        },
-                        0xE => {            // 8XYE Stores the most significant bit of VX in VF and then shifts VX to the left by 1 
-                            vreg[0xf] = (vreg[n2] & 0x80) >> 7;
-                            vreg[n2] <<= 1;
-                            *pc += 2;
-                        },
-                        _ => { println!("Invalid Instruction: {:#06x}", ins);
-                               *pc += 2;
-                             }
-                  }
+        0x1000 => *pc = ins & 0x0fff,   // 1NNN - Jump to NNN
+        0x2000 => {                     // 2NNN Call subroutine at address NNN
+            stack.push(*pc);         
+            *pc = ins & 0x0fff;
+        },        
+        0x3000 => {                     // 3XNN Skip next instruction if VX = NN  
+            *pc += if vreg[n2] == (ins & 0xff) as u8 
+            { 4 } else { 2 };         
+        },                         
+        0x4000 => {                     // 4XNN Skip next instruction if VX != NN
+            *pc += if vreg[n2] != (ins & 0xff) as u8 
+            { 4 } else { 2 };
         },
-        0x9000 => { *pc += if vreg[n2] != vreg[n3]
-                    { 4 } else { 2 };       // 9XY0 Skips the next instruction if VX doesn't equal VY 
-                  },                        
-        0xA000 => { *ireg = ins & 0x0fff;
-                    *pc += 2;               // ANNN Set I to NNN
-                  },
-        0xB000 => { *pc = (ins & 0x0fff) + vreg[0] as u16; },        // BNNN Jump to address NNN + V0
-        0xC000 => { let randon_number = rand::random::<u8>();
-                    vreg[n2] = randon_number & (ins & 0x00ff) as u8;
-                  },           // CXNN Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
-        0xD000 => {  },        // DXYN Draw a sprite at VX,YV that has a width of 8px and height of Npx
-        0xE000 => { match ins & 0x00ff {
-                        0x009E => { // EX9E Skips the next instruction if the key stored in VX is pressed.
-                            *pc += if vreg[n2] == *key_id
-                            { 4 } else { 2 }
-                        },
-                        0x00A1 => { // EXA1 Skips the next instruction if the key stored in VX isn't pressed.
-                            *pc += if vreg[n2] != *key_id
-                            { 4 } else { 2 }
-                        },
-                        _ => { println!("Invalid Instruction: {:#06x}", ins);
-                               *pc += 2;
-                             }
-                  }
+        0x5000 => {                     // 5XY0 Skip next instruction if VX = VY
+            *pc += if vreg[n2] == vreg[n3]
+            { 4 } else { 2 };
         },
-        0xF000 => { match ins & 0x00ff {
-                        0x07 => { // FX07 Sets VX to the value of the delay timer.
-                            vreg[n2] = *delay_timer;
-                            *pc += 2;
-                        },
-                        0x0A => {} // FX0A A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event)
-                        0x15 => { // FX15 Sets the delay timer to VX.                                      
-                            *delay_timer = vreg[n2];
-                            *pc += 2;
-                        },
-                        0x18 => { // FX18 Sets the sound timer to VX.                                      
-                            *sound_timer = vreg[n2];
-                            *pc += 2;
-                        },
-                        0x1E => { // FX1E Add Vx to I                                      
-                            *ireg += vreg[n2] as u16;
-                            *pc += 2;
-                        },
-                        0x29 => { // FX29 Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.                                      
-                            *ireg = 0x0050 + vreg[n2] as u16 * 5;
-                            *pc += 2;
-                        },
-                        _ => {}
-                  }
+        0x6000 => {                     // 6XNN - Set VX to NN
+            vreg[n2] = (ins & 0x00ff) as u8; 
+            *pc += 2; 
+        },
+        0x7000 => {                     // 7XNN Add NN to VX (Carry flag not changed)  
+            vreg[n2] = vreg[n2] + (ins & 0xff) as u8;
+            *pc += 2; 
+        },
+        0x8000 => { 
+            match n4 {
+                0x0 => {                // 8XY0 Sets the value of VX to VY      
+                    vreg[n2] = vreg[n3];
+                    *pc += 2;
+                },
+                0x1 => {                // 8XY1 Sets VX to VX or VY (Bitwise OR operation) 
+                    vreg[n2] |= vreg[n3];
+                    *pc += 2;
+                },
+                0x2 => {                // 8XY2 Sets VX to VX & VY (Bitwise AND operation) 
+                    vreg[n2] &= vreg[n3];
+                    *pc += 2;
+                },
+                0x3 => {                // 8XY3 Sets VX to VX XOR VY (Bitwise OXR operation) 
+                    vreg[n2] ^= vreg[n3];
+                    *pc += 2;
+                },
+                0x4 => {                // 8XY4 Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't. 
+                    let sum: u16 = vreg[n2] as u16 + vreg[n3] as u16;
+                    vreg[0xf] = if sum > 255 { 1 } else { 0 };
+                    vreg[n2] = sum as u8;
+                    *pc += 2;
+                },
+                0x5 => {                // 8XY5 VX = VX - VY. VF is set to 0 when there's a borrow, and 1 when there isn't.
+                    let difference: i16 = vreg[n2] as i16 - vreg[n3] as i16;
+                    if difference >= 0 {
+                        vreg[n2] = difference as u8;
+                        vreg[0xf] = 1;
+                    } else {
+                        vreg[n2] = (difference + 256) as u8;
+                        vreg[0xf] = 0;
+                    }
+                    *pc += 2;
+                },
+                0x6 => {                // 8XY6 Stores the least significant bit of VX in VF and then shifts VX to the right by 1 
+                    vreg[0xf] = vreg[n2] & 0x01;
+                    vreg[n2] >>= 1; 
+                    *pc += 2;
+                },
+                0x7 => {                // 8XY7 VX = VY - VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
+                    let difference: i16 = vreg[n3] as i16 - vreg[n2] as i16;
+                    if difference >= 0 {
+                        vreg[n2] = difference as u8;
+                        vreg[0xf] = 1;
+                    } else {
+                        vreg[n2] = (difference + 256) as u8;
+                        vreg[0xf] = 0;
+                    }
+                    *pc += 2;
+                },
+                0xE => {                // 8XYE Stores the most significant bit of VX in VF and then shifts VX to the left by 1 
+                    vreg[0xf] = (vreg[n2] & 0x80) >> 7;
+                    vreg[n2] <<= 1;
+                    *pc += 2;
+                },
+                _ => { println!("Invalid Instruction: {:#06x}", ins);
+                        *pc += 2;
+                }
+            }
+        },
+        0x9000 => {                     // 9XY0 Skips the next instruction if VX doesn't equal VY 
+            *pc += if vreg[n2] != vreg[n3]
+            { 4 } else { 2 };
+        },                        
+        0xA000 => {                     // ANNN Set I to NNN
+            *ireg = ins & 0x0fff;
+            *pc += 2;
+        },
+        0xB000 => *pc = (ins & 0x0fff) + vreg[0] as u16,        // BNNN Jump to address NNN + V0
+        0xC000 => {                     // CXNN Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
+            let randon_number = rand::random::<u8>();
+            vreg[n2] = randon_number & (ins & 0x00ff) as u8;
+        },
+        0xD000 => {  },                 // DXYN Draw a sprite at VX,YV that has a width of 8px and height of Npx
+        0xE000 => { 
+            match ins & 0x00ff {
+                0x009E => {             // EX9E Skips the next instruction if the key stored in VX is pressed.
+                    *pc += if vreg[n2] == *key_id
+                    { 4 } else { 2 }
+                },
+                0x00A1 => {             // EXA1 Skips the next instruction if the key stored in VX isn't pressed.
+                    *pc += if vreg[n2] != *key_id
+                    { 4 } else { 2 }
+                },
+                _ => { println!("Invalid Instruction: {:#06x}", ins);
+                        *pc += 2;
+                }
+            }
+        },
+        0xF000 => { 
+            match ins & 0x00ff {
+                0x07 => {               // FX07 Sets VX to the value of the delay timer.
+                    vreg[n2] = *delay_timer;
+                    *pc += 2;
+                },
+                0x0A => {}              // FX0A A key press is awaited, and then stored in VX. (Blocking Operation. All instruction halted until next key event)
+                0x15 => {               // FX15 Sets the delay timer to VX.                                      
+                    *delay_timer = vreg[n2];
+                    *pc += 2;
+                },
+                0x18 => {               // FX18 Sets the sound timer to VX.                                      
+                    *sound_timer = vreg[n2];
+                    *pc += 2;
+                },
+                0x1E => {               // FX1E Add Vx to I                                      
+                    *ireg += vreg[n2] as u16;
+                    *pc += 2;
+                },
+                0x29 => {               // FX29 Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font.                                      
+                    *ireg = 0x0050 + vreg[n2] as u16 * 5;
+                    *pc += 2;
+                },
+                _ => {}
+            }
         },
         _ => {}
     }
